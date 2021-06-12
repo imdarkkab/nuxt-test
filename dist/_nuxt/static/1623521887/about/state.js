b414cb6e5661d5f8fd37c8c177eb8bf084c766fd
thawalit <thawalit@gmail.com>
2021-06-13 01:37:17 +0700

    ok

--- dist/_nuxt/static/1623521887/about/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnext-test\u002F_nuxt\u002Fstatic\u002F1623521887",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fabout",config:{_app:{basePath:"\u002Fnext-test\u002F",assetsPath:"\u002Fnext-test\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b25b294c"}}}}(null));
